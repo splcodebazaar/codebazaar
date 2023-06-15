eec4eb093b51e5a04afbf05ec4b0becc0d3cfd74
Siddharth Jain Singhai <singhaisiddharth58@gmail.com>
2023-06-15 15:06:53 +0530

    changed partialized

--- com.codebazaar/codebazaar_Apps/staticFiles/JavaScript files/header.js
+++ com.codebazaar/codebazaar_Apps/staticFiles/JavaScript files/header.js
@@ -1,6 +1,6 @@
 /* =======header Js======= */
 
-// creating menu toggle for phone/ipads devices
+// creating menu togglle controllers
 let navigation = document.getElementById('menu');
 let menuOn = document.getElementById("show-menu");
 let menuOff = document.getElementById("hide-menu");
@@ -20,4 +20,27 @@ function menuHide() {
     menuOn.style.display = 'initial';
     menuOff.style.display = 'none';
     navigation.style.display = 'none';
+}
+
+
+
+// function for activate Theme-mode
+let themes = document.getElementById('modes');
+let darkMode = document.getElementById('dark');
+let lightMode = document.getElementById('light');
+
+themes.addEventListener('click', themeMode);
+
+// function for activate theme-modes
+function themeMode() {
+    
+    if(lightMode.style.display != 'none') {
+        darkMode.style.display = 'initial';
+        lightMode.style.display = 'none';
+    }
+
+    else {
+        darkMode.style.display = 'none';
+        lightMode.style.display = 'initial';
+    }
 }
